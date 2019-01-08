badf7dec3e0b754147f6eb39a2c164db164226cd
Mihai Osvat <mihai.osvat@nordlogic.com>
2019-01-09 00:10:59 +0200

    Fixes

--- src/app/entities/entities.controller.ts
+++ src/app/entities/entities.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 
 import { EntitiesService } from './entities.service';
 import { OrganisationDto } from '../models/organisation.dto';
--- src/app/entities/entities.service.ts
+++ src/app/entities/entities.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../../shared/database/database.service';
 import { EntityDto } from '../models/entity.dto';
 import { EntityModel } from '../models/entity.model';
+import { ObjectType } from 'typeorm';
 
 @Injectable()
 export class EntitiesService {
     constructor(private databaseService: DatabaseService) {}
 
-    async createEntity(model: EntityDto, entity: EntityDto): Promise<any> {
+    async createEntity(model: ObjectType<EntityModel>, entity: EntityDto): Promise<any> {
         this.databaseService.add(model, entity);
     }
 }
