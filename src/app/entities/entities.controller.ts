262ab3961e7bdfca6cbb6c9b06734bb25829aad5
Mihai Osvat <mihai.osvat@nordlogic.com>
2019-01-09 00:06:37 +0200

    Eliminated calls to UsePipes on controller

--- src/app/entities/entities.controller.ts
+++ src/app/entities/entities.controller.ts
@@ -11,13 +11,11 @@ export class EntitiesController {
     constructor(private service: EntitiesService) {}
 
     @Post('/organisations')
-    @UsePipes(new ValidationPipe())
     async createOrganisation(@Body() dto: OrganisationDto) {
         await this.service.createEntity(OrganisationModel, dto);
     }
 
     @Post('/standards')
-    @UsePipes(new ValidationPipe())
     async createStandard(@Body() dto: StandardDto) {
         await this.service.createEntity(StandardModel, dto);
     }
