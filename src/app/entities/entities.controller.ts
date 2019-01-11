d1cc5041ecc8e626c7189680f53779bcaf7c868a
Mihai Osvat <mihai.osvat@nordlogic.com>
2019-01-11 22:24:24 +0200

    Added missing post entities

--- src/app/entities/entities.controller.ts
+++ src/app/entities/entities.controller.ts
@@ -5,6 +5,10 @@ import { OrganisationDto } from '../models/organisation.dto';
 import { OrganisationModel } from '../models/organisation.model';
 import { StandardDto } from '../models/standard.dto';
 import { StandardModel } from '../models/standard.model';
+import { DomainDto } from '../models/domain.dto';
+import { DomainModel } from '../models/domain.model';
+import { LocationDto } from '../models/location.dto';
+import { LocationModel } from '../models/location.model';
 
 @Controller('entities')
 export class EntitiesController {
@@ -18,5 +22,15 @@ export class EntitiesController {
     @Post('/standards')
     async createStandard(@Body() dto: StandardDto) {
         await this.service.createEntity(StandardModel, dto);
+    }
+
+    @Post('/domains')
+    async createDomain(@Body() dto: DomainDto) {
+        await this.service.createEntity(DomainModel, dto);
+    }
+
+    @Post('/locations')
+    async createLocation(@Body() dto: LocationDto) {
+        await this.service.createEntity(LocationModel, dto);
     }
 }
