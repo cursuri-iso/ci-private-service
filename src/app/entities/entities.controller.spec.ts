f314451beefbe13cb4c6f1416c5b6bf5e4ff1b5e
Mihai Osvat <mihai.osvat@nordlogic.com>
2019-02-03 11:02:51 +0200

    Started writing tests

--- src/app/entities/entities.controller.spec.ts
+++ src/app/entities/entities.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntitiesController } from './entities.controller';
+import { SharedModule } from '../../shared/shared.module';
+import { EntitiesService } from './entities.service';
 
 describe('Entities Controller', () => {
   let module: TestingModule;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
+      imports: [SharedModule],
+      providers: [EntitiesService],
       controllers: [EntitiesController],
     }).compile();
   });
--- src/app/entities/entities.service.spec.ts
+++ src/app/entities/entities.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntitiesService } from './entities.service';
+import { SharedModule } from '../../shared/shared.module';
 
 describe('EntitiesService', () => {
   let service: EntitiesService;
-  
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [SharedModule],
       providers: [EntitiesService],
     }).compile();
     service = module.get<EntitiesService>(EntitiesService);
